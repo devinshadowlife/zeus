1dc5fb22f84a7b7199436948288c1d08014dcabc
Raina Moon <mds6425@gmail.com>
2025-06-06 20:31:56 +0900

    Add animations and improve component visibility with Intersection Observer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-        "./src/**/*.{js,ts,jsx,tsx,css}",
-
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
   theme: {
     extend: {
+      keyframes: {
+        "fade-up": {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "fade-down": {
+          "0%": { opacity: "0", transform: "translateY(-20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-right": {
+          "0%": { opacity: "0", transform: "translateX(-30px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        "fade-left": {
+          "0%": { opacity: "0", transform: "translateX(30px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        "fade-up": "fade-up 0.8s ease-out forwards",
+        "fade-down": "fade-down 0.8s ease-out forwards",
+        "fade-right": "fade-right 0.8s ease-out forwards",
+        "fade-left": "fade-left 0.8s ease-out forwards",
+      },
+
       fontFamily: {
-        cinzel: ['var(--font-cinzel)'],
-        lora: ['var(--font-lora)'],
+        cinzel: ["var(--font-cinzel)"],
+        lora: ["var(--font-lora)"],
       },
     },
   },
